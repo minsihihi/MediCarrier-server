ee65c65f5589622ffa6d0821b9a0c791d466a995
minsihihi <qkralstj@gmail.com>
2024-07-20 23:43:57 +0900

    create superuser

--- server/server/medicarrier/models.py
+++ server/server/medicarrier/models.py
@@ -1,19 +1,22 @@
+# medicarrier/models.py
 from django.db import models
 from django.conf import settings
 
-# Create your models here.
+
 class Trip(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     country = models.CharField(max_length=20)
-    start_date = models.DateTimeField
-    end_date = models.DateTimeField
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
 
     def __str__(self):
         return self.country
 
 
 class Assist(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
     facility = models.CharField(max_length=20)
     hospital_type = models.CharField(max_length=20)
     symptom_type = models.CharField(max_length=20)
@@ -41,25 +44,31 @@ class Hospital(models.Model):
     def __str__(self):
         return self.hospital_name
 
+
 class Insurance(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     insurance_type = models.CharField(max_length=20)
     insturance_name = models.CharField(max_length=20)
     insurance_call = models.CharField(max_length=20)
 
     def __str__(self):
         return self.insturance_name
-    
+
+
 class MediCard(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     country = models.OneToOneField(Trip, on_delete=models.CASCADE)
     language = models.CharField(max_length=20)
 
     def __str__(self):
-        return self.country
+        return str(self.country)
+
 
 class MediInfo(models.Model):
-    medicard = models.OneToOneField(MediCard, on_delete=models.CASCADE, primary_key=True)
+    medicard = models.OneToOneField(
+        MediCard, on_delete=models.CASCADE, primary_key=True)
     condition = models.CharField(max_length=20, default="현재 증상 없음")
     illness = models.CharField(max_length=20, default="없음")
     medicine = models.CharField(max_length=20, default="복용하는 약 없음")
@@ -68,10 +77,12 @@ class MediInfo(models.Model):
     surgery = models.CharField(max_length=20, default="근 n개월 이내 없음")
 
     def __str__(self):
-        return self.medicard
-    
+        return str(self.medicard)
+
+
 class BasicInfo(models.Model):
-    medicard = models.OneToOneField(MediCard, on_delete=models.CASCADE, primary_key=True)
+    medicard = models.OneToOneField(
+        MediCard, on_delete=models.CASCADE, primary_key=True)
     name = models.CharField(max_length=20)
     sex = models.CharField(max_length=20)
     nationality = models.CharField(max_length=20)
@@ -83,5 +94,4 @@ class BasicInfo(models.Model):
     pregnant = models.CharField(max_length=20)
 
     def __str__(self):
-        return self.medicard
-
+        return str(self.medicard)
