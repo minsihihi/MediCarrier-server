ae53e63c3ce36455454cfa81b816c6242fe53392
bbandm <hjerachel@gmail.com>
2024-07-20 23:44:06 +0900

    시리얼라이저

--- server/server/medicarrier/admin.py
+++ server/server/medicarrier/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Trip)
+admin.site.register(Assist)
+admin.site.register(Hospital)
+admin.site.register(Insurance)
+admin.site.register(MediCard)
+admin.site.register(MediInfo)
+admin.site.register(BasicInfo)
