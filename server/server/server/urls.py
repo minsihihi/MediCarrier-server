0293ba7b4d1baa1e7b112b1942e85837365479ce
bbandm <hjerachel@gmail.com>
2024-07-21 20:19:41 +0900

    충돌 해결

--- server/server/server/urls.py
+++ server/server/server/urls.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from django.urls import path , include
-=======
 from django.urls import path, include
 
->>>>>>> f6fc0956b7d273d6e8c73e0ff41513a500046c2d
 
 urlpatterns = [
     path('admin/', admin.site.urls),
