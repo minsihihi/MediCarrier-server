6bd4d9ee86743cd6253cca14323fa7af31e8185d
bbandm <hjerachel@gmail.com>
2024-08-01 00:35:58 +0900

    배포 전 푸시

--- server/server/server/urls.py
+++ server/server/server/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('medicarrier-EwhaLikeLion12-admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('medicarrier/', include('medicarrier.urls')),
 ]
